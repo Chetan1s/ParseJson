c648a3c068774502d9ab7329ca9b1c35f2bfd377
chetan <chetan.sharma871996@gmail.com>
2021-05-15 20:44:19 +0530

    Modified

--- First-node/task.js
+++ First-node/task.js
@@ -3,10 +3,11 @@ const safeEval = require('safe-eval'),
 
 
 
-const constructMessage = async function(template='', data={}, templateVersion=1) {
+const constructMessage =  function(template='', data={}, templateVersion=1) {
 	try {
         //replace the rest of the variables
         const variablesInExpression = template.match(/{{(.*?)}}/g);
+       // console.log(variablesInExpression);
         var x;
         // console.log('variablesInExpression -> ', variablesInExpression);
         if(Array.isArray(variablesInExpression)) {
@@ -16,25 +17,21 @@ const constructMessage = async function(template='', data={}, templateVersion=1)
                 // console.log('jmesPathValue -> ', jmespath.search(data.variables, jmesPathExpression));
                 const pattern = new RegExp(variable, 'g');
                 var v=(jmespath.search(data, jmesPathExpression));
-                if(v===null){
-                x='NA';}
+               
                // console.log(v,typeof v);
+                if(v===null){
+                x='N';
+                return ;
+            }
                 template = template.replace(pattern, jmespath.search(data, jmesPathExpression));
+               // console.log(template,typeof template);
                 // console.log('template -> ', template);
             });
         }
-        if(x==='NA')
-        return await "N";
-        var templateJSON=JSON.parse(template);
-        //console.log(templateJSON);
-        var ans;
-        
-        Object.keys(templateJSON).forEach(function(key) {
-            var value = templateJSON[key];
-            ans=safeEval(value);
-        });
-
-        return await ans;
+      // console.log(template,typeof template);
+        if(x==='N')
+        return 'N';
+        return  safeEval(template);
        
 	} catch(err) {
 		console.log(err);
@@ -42,7 +39,7 @@ const constructMessage = async function(template='', data={}, templateVersion=1)
 };
 
 var JSON2={
-    "voltage": "215.45",
+   "voltage": "215.45",
     "current": "3.46",
     "power_factor": "0.98",
     "pressure": "1024.56"
@@ -58,26 +55,16 @@ var JSON1={
 var out=str;
 var output=JSON.parse(out);
 
-async function fun (){
 Object.keys(JSON1).forEach(function(key) {
-    var value = JSON1[key];
-    var str='{"'+key+'"'+':"'+value+'"}';
-    
-    //console.log(str,typeof str);
-    var count=0;
-    let ans;
-    (async ()=>{
-        ans=await constructMessage(str.toString(),data);
-        if(ans==='N'){}
+    var value = JSON1[key];   
+        ans= constructMessage(value,data);
+        //console.log(ans,typeof ans);
+
+        if(ans=='N'){}
         else{
         output[key]=parseFloat(ans).toFixed(4);}
-    })()
 });
-return await "" ;
-}
-(async()=>{
-    await fun();
     console.log("input->",data)
     console.log("output->",output);
-})()
+
 
